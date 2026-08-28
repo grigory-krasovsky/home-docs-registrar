bcaa38f4684123d7374d99fcb30e823814c581d8
krasovsky <krasovsky@elbrus.ru>
2026-08-28 16:11:49 +0300

    initial commit

---
+++ src/main/java/com/example/homedocsregistrar/HomeDocsRegistrarApplication.java
@@ -0,0 +1,13 @@
+package com.example.homedocsregistrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeDocsRegistrarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeDocsRegistrarApplication.class, args);
+    }
+
+}
